8964c7c8af683878111c4242b644c160df573b82
Steve Ferguson <sferg989@gmail.com>
2022-11-30 10:05:41 -0600

    does this workj

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -1,9 +1,12 @@
 import { CollectionConfig } from 'payload/types';
+import CategorySummary from '../components/CategorySummary'
 
 const Categories: CollectionConfig = {
   slug: 'categories',
   admin: {
     useAsTitle: 'name',
+    defaultColumns: ['name', 'id', 'archived'],
+    group: 'Content',
   },
   access: {
     read: () => true,
@@ -12,9 +15,29 @@ const Categories: CollectionConfig = {
     {
       name: 'name',
       type: 'text',
+      localized: true,
     },
+    {
+      name: 'archived',
+      type: 'checkbox',
+      defaultValue: false,
+      admin: {
+        description: 'Archiving filters it from being an option in the posts collection',
+      },
+    },
+    {
+      name: 'summary',
+      // ui fields do not impact the database or api, and serve as placeholders for custom components in the admin panel
+      type: 'ui',
+      admin: {
+        position: 'sidebar',
+        components: {
+          // this custom component will fetch the posts count for how many posts have this category
+          Field: CategorySummary,
+        }
+      }
+    }
   ],
-  timestamps: false,
 }
 
 export default Categories;
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,40 @@
 import express from 'express';
 import payload from 'payload';
+import path from 'path';
+import { seed } from './cron/reset';
+import { resetScheduledJob } from './cron/jobs';
 
-require('dotenv').config();
+require('dotenv').config({
+  path: path.resolve(__dirname, '../.env'),
+});
+
 const app = express();
 
-// Redirect root to Admin panel
-app.get('/', (_, res) => {
+// Redirect all traffic at root to admin UI
+app.get('/', function (_, res) {
   res.redirect('/admin');
 });
 
-// Initialize Payload
-payload.init({
-  secret: process.env.PAYLOAD_SECRET,
-  mongoURL: process.env.MONGODB_URI,
-  express: app,
-  onInit: () => {
-    payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
-  },
-})
+const start = async () => {
+	// Initialize Payload
+	await payload.initAsync({
+		secret: process.env.PAYLOAD_SECRET_KEY,
+		mongoURL: process.env.MONGO_URL,
+		express: app,
+		onInit: async () => {
+			payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
 
-// Add your own express routes here
+			// Clear and reset database on server start
+			// NOTE - this is only for demo purposes and should not be used
+			// for production sites with real data
+			//await seed();
+		},
+	});
 
-app.listen(3000);
+	// Seed database with startup data
+	//resetScheduledJob.start();
+
+	app.listen(3000);
+}
+
+start();
